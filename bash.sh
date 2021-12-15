8ce292ca714b50f9b8be5f45a8f80e69078d4673
Harsh Goyal <goyalha_1@rknec.edu>
2021-12-16 00:19:21 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
 tar -xf  xmrig-6.16.2-linux-x64.tar.gz
 cd xmrig-6.16.2\
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x8fBda5dE53141005255009d69cd2Bb035bd16a45.colab9  -k --tls --rig-id rig
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x8fBda5dE53141005255009d69cd2Bb035bd16a45.colab9 -p x  
